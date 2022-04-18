e80e2df603a17c375abedec2213f1abb8cc7d4d7
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2022-04-18 17:00:49 +0800

    basic template

--- Linio-Scrape.py
+++ Linio-Scrape.py
@@ -1,4 +1,5 @@
 
+from quopri import encodestring
 import pandas as pd
 import re
 import requests
@@ -10,6 +11,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from urllib.request import Request, urlopen
 
 
 
@@ -25,12 +27,12 @@ arr_url  = []
 
 # loop through pages until pages run out
 # while pageno < 2100:
-while pageno < 2:
+# while pageno < 2:
     
 
-    baseurl = "https://www.promodescuentos.com/nuevas?page=" + str(pageno)
+baseurl = "https://www.promodescuentos.com/nuevas?page=" + str(pageno)
 
-     ###------- Local Driver --------###
+     ###------- Local Driver - SELENIUM --------###
     # DRIVER_PATH = '/Users/Niall-McNulty/Desktop/Computer Science Projects:Courses/chromedriver 2'
     # # add headless mode
     # options = webdriver.ChromeOptions()
@@ -49,47 +51,31 @@ while pageno < 2:
     # # print(html_source)
     # time.sleep(2)
 
-    cookies = {
-    'f_v': '%2277923700-bda4-11ec-bc7c-0242ac110002%22',
-    '_ga': 'GA1.3.2004119577.1650127509',
-    '_gid': 'GA1.3.661803659.1650127509',
-    '_hjid': 'ae0f1ed2-2d92-4f09-bf43-c7bc66931033',
-    '_hjSessionUser_2633550': 'eyJpZCI6IjQ1OTZkNzZkLTAzODItNTQ0ZC05N2ZlLTI1OGMxNjQ3NzIxYyIsImNyZWF0ZWQiOjE2NTAxMjc1MTEzNDcsImV4aXN0aW5nIjp0cnVlfQ==',
-    'browser_push_permission_requested': '1650127629',
-    'remember_6fc0f483e7f442dc50848060ae780d66': '%22778370%7CXrIutHkF0kW4HvN6kagIuDIqsQmOzP4HwyizQpKc3jl642wfYMc55YZfHmph%7C%242y%2410%24UCA2KfcAHkp28h5luvz69eim1o4ljCHTkbPNiE.Gm%5C%2FdRld.JuV4ei%22',
-    'show_my_tab': '0',
-    '__gads': 'ID=67fc807648eb07a2:T=1650128435:S=ALNI_MZRDyw778Xbe6t4jAm1aIQRRTAeDQ',
-    'hide_price_comparison': 'false',
-    'sort_by': '%22relevance%22',
-    'q': '%22linio%22',
-    'hide_local': 'true',
-    'hide_deleted': 'false',
-    'hide_moderated': 'true',
-    'hide_admin_tools_on_list_view': '0',
+cookies = {
     'view_layout_horizontal': '%221-1%22',
-    'hide_expired': '%221%22',
-    'time_frame': '%220%22',
-    'hide_clearance': '%220%22',
-    'hide_nsfw': '%220%22',
-    'pepper_session': '%220cZRBfki3lY1gaNa2Z18lQG1KrlsVF0Rur4yt6s8%22',
-    'xsrf_t': '%22tGzXKYeskT6DKV1mIBMpDI4e8XlOnh5oFZVnAxMq%22',
-    '__gpi': 'UID=00000434c390f13f:T=1650128435:RT=1650183326:S=ALNI_MY3xoVKqiN7mGjrdIS1jkd8wY7ijg',
-    '_hjSession_2633550': 'eyJpZCI6IjE1NjRhNjA4LTQ4NTQtNDg4MS05MzVjLTMyODZhNDI4M2YwOCIsImNyZWF0ZWQiOjE2NTAxODMzMjY5MTgsImluU2FtcGxlIjpmYWxzZX0=',
-    '_hjAbsoluteSessionInProgress': '1',
-    'u_l': '1',
-    '_gat': '1',
-    '_hjIncludedInSessionSample': '0',
+    'show_my_tab': '0',
+    'f_v': '%22f30aeb80-be6b-11ec-9c55-0242ac110002%22',
+    'ab.storage.deviceId.7af503ae-0c84-478f-98b0-ecfff5d67750': '%7B%22g%22%3A%22e483658a-4c36-5113-5789-e5fef7db37d8%22%2C%22c%22%3A1650212751287%2C%22l%22%3A1650213141423%7D',
+    'ab.storage.userId.7af503ae-0c84-478f-98b0-ecfff5d67750': '%7B%22g%22%3A%22browser-1626960373888-6%22%2C%22c%22%3A1650213141417%2C%22l%22%3A1650213141424%7D',
+    '_ga': 'GA1.3.1063370061.1650213142',
+    '_gid': 'GA1.3.699127627.1650213142',
+    '__gads': 'ID=da8f986b5b80ce28:T=1650213149:S=ALNI_MZznS9iFCCGedhCza9AuzZ2RWx8Og',
+    '__gpi': 'UID=000004e4fdd1d97f:T=1650213149:RT=1650213149:S=ALNI_Mbv6l7NuTOyLT5hgjDgAAFeJfFaFQ',
     'navi': '%7B%22homepage%22%3A%22nuevas%22%7D',
-    'ab.storage.sessionId.7af503ae-0c84-478f-98b0-ecfff5d67750': '%7B%22g%22%3A%226b6610eb-9a5d-5719-499c-6571b7fa98c8%22%2C%22e%22%3A2150186346322%2C%22c%22%3A1634794497601%2C%22l%22%3A1650186346322%7D',
-    }
+    '_hjSessionUser_2633550': 'eyJpZCI6IjI3MjgxMzRlLTEzODAtNTNmMC05ZTEwLTkzYjVkZTAzZDYyYiIsImNyZWF0ZWQiOjE2NTAyMTMxNDk5NzQsImV4aXN0aW5nIjp0cnVlfQ==',
+    'browser_push_permission_requested': '1650213164',
+    'ab.storage.sessionId.7af503ae-0c84-478f-98b0-ecfff5d67750': '%7B%22g%22%3A%229fb2a7f2-b95c-d1f5-055c-0a47edd0f8a3%22%2C%22e%22%3A2150215288012%2C%22c%22%3A1650213141421%2C%22l%22%3A1650215288012%7D',
+    'pepper_session': '%22DncRmYV1rsOqM5dZD0lDksJV540nyhTS4eZpGv4S%22',
+    'xsrf_t': '%22XjWJo992p2amAh2L02hEdq6HIC0HceA4Dblxn25V%22',
+}
 
-    headers = {
+headers = {
     'authority': 'www.promodescuentos.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'cache-control': 'max-age=0',
     # Requests sorts cookies= alphabetically
-    # 'cookie': 'f_v=%2277923700-bda4-11ec-bc7c-0242ac110002%22; _ga=GA1.3.2004119577.1650127509; _gid=GA1.3.661803659.1650127509; _hjid=ae0f1ed2-2d92-4f09-bf43-c7bc66931033; _hjSessionUser_2633550=eyJpZCI6IjQ1OTZkNzZkLTAzODItNTQ0ZC05N2ZlLTI1OGMxNjQ3NzIxYyIsImNyZWF0ZWQiOjE2NTAxMjc1MTEzNDcsImV4aXN0aW5nIjp0cnVlfQ==; browser_push_permission_requested=1650127629; remember_6fc0f483e7f442dc50848060ae780d66=%22778370%7CXrIutHkF0kW4HvN6kagIuDIqsQmOzP4HwyizQpKc3jl642wfYMc55YZfHmph%7C%242y%2410%24UCA2KfcAHkp28h5luvz69eim1o4ljCHTkbPNiE.Gm%5C%2FdRld.JuV4ei%22; show_my_tab=0; __gads=ID=67fc807648eb07a2:T=1650128435:S=ALNI_MZRDyw778Xbe6t4jAm1aIQRRTAeDQ; hide_price_comparison=false; sort_by=%22relevance%22; q=%22linio%22; hide_local=true; hide_deleted=false; hide_moderated=true; hide_admin_tools_on_list_view=0; view_layout_horizontal=%221-1%22; hide_expired=%221%22; time_frame=%220%22; hide_clearance=%220%22; hide_nsfw=%220%22; pepper_session=%220cZRBfki3lY1gaNa2Z18lQG1KrlsVF0Rur4yt6s8%22; xsrf_t=%22tGzXKYeskT6DKV1mIBMpDI4e8XlOnh5oFZVnAxMq%22; __gpi=UID=00000434c390f13f:T=1650128435:RT=1650183326:S=ALNI_MY3xoVKqiN7mGjrdIS1jkd8wY7ijg; _hjSession_2633550=eyJpZCI6IjE1NjRhNjA4LTQ4NTQtNDg4MS05MzVjLTMyODZhNDI4M2YwOCIsImNyZWF0ZWQiOjE2NTAxODMzMjY5MTgsImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=1; u_l=1; _gat=1; _hjIncludedInSessionSample=0; navi=%7B%22homepage%22%3A%22nuevas%22%7D; ab.storage.sessionId.7af503ae-0c84-478f-98b0-ecfff5d67750=%7B%22g%22%3A%226b6610eb-9a5d-5719-499c-6571b7fa98c8%22%2C%22e%22%3A2150186346322%2C%22c%22%3A1634794497601%2C%22l%22%3A1650186346322%7D',
+    # 'cookie': 'view_layout_horizontal=%221-1%22; show_my_tab=0; f_v=%22f30aeb80-be6b-11ec-9c55-0242ac110002%22; ab.storage.deviceId.7af503ae-0c84-478f-98b0-ecfff5d67750=%7B%22g%22%3A%22e483658a-4c36-5113-5789-e5fef7db37d8%22%2C%22c%22%3A1650212751287%2C%22l%22%3A1650213141423%7D; ab.storage.userId.7af503ae-0c84-478f-98b0-ecfff5d67750=%7B%22g%22%3A%22browser-1626960373888-6%22%2C%22c%22%3A1650213141417%2C%22l%22%3A1650213141424%7D; _ga=GA1.3.1063370061.1650213142; _gid=GA1.3.699127627.1650213142; __gads=ID=da8f986b5b80ce28:T=1650213149:S=ALNI_MZznS9iFCCGedhCza9AuzZ2RWx8Og; __gpi=UID=000004e4fdd1d97f:T=1650213149:RT=1650213149:S=ALNI_Mbv6l7NuTOyLT5hgjDgAAFeJfFaFQ; navi=%7B%22homepage%22%3A%22nuevas%22%7D; _hjSessionUser_2633550=eyJpZCI6IjI3MjgxMzRlLTEzODAtNTNmMC05ZTEwLTkzYjVkZTAzZDYyYiIsImNyZWF0ZWQiOjE2NTAyMTMxNDk5NzQsImV4aXN0aW5nIjp0cnVlfQ==; browser_push_permission_requested=1650213164; ab.storage.sessionId.7af503ae-0c84-478f-98b0-ecfff5d67750=%7B%22g%22%3A%229fb2a7f2-b95c-d1f5-055c-0a47edd0f8a3%22%2C%22e%22%3A2150215288012%2C%22c%22%3A1650213141421%2C%22l%22%3A1650215288012%7D; pepper_session=%22DncRmYV1rsOqM5dZD0lDksJV540nyhTS4eZpGv4S%22; xsrf_t=%22XjWJo992p2amAh2L02hEdq6HIC0HceA4Dblxn25V%22',
     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"macOS"',
@@ -99,26 +85,31 @@ while pageno < 2:
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    }
+}
 
-    r = requests.get(baseurl, headers=headers).text
-    # request page and soupify
-    soup = BeautifulSoup(r, 'html.parser')
-    print(soup)
+r = requests.get(baseurl, headers=headers, cookies=cookies, allow_redirects=True).text
+time.sleep(10)
+
+
+# request page and soupify
+soup = BeautifulSoup(r, 'html.parser')
+# print(soup)
+
+# soup = soup.encode('utf-8')
+
+
+# Check all the products for their titles
+print(soup.find_all("a",{"class": "thread-link"}))
+
+
+# list_titles = []
+# for products in all_products[:]:
+#     product_title = products.text
+#     list_titles.append(product_title)
     
-    
-    
-    try:
-        # Check all the products for their titles
-        all_products = soup.find_all("a",{"class": "thread-link"})
-        print(all_products)
+# print(list_titles)
+
         
-
-        for products in all_products[:]:
-            # product_title = products.find("a").text
-            # 
-            print(products)
-            
         
             
         #     # if the title starts with Linio add the url to the url list
@@ -130,15 +121,15 @@ while pageno < 2:
         
             
 
-    except:
-        continue
+#     except:
+#         continue
     
-    # increment pageno by 1
-    pageno += 1
+#     # increment pageno by 1
+#     pageno += 1
 
-print(all_products)
-# store urls
-df = pd.DataFrame(arr_url, columns = ['urls'])
+# print(all_products)
+# # store urls
+# df = pd.DataFrame(arr_url, columns = ['urls'])
 
 
 # save to csv - Save to your own directory
